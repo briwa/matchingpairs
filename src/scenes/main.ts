aec2f37a414079e35209a467e21aa61d03a04aec
briwa <shabriwa.shalat@gmail.com>
2020-01-01 01:10:08 +0800

    refactor(sprites): even better logic

--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -6,10 +6,11 @@ export default class MainScene extends Phaser.Scene {
     super({ key: 'MainScene' })
   }
 
-  private size = 8
+  private size = 4
   private tileSize = 32
   private zoomFactor = 1.5
   private level: Emoji[][] = []
+  private openEmojis: Emoji[] = []
 
   preload () {
     // 42 columns * 44 rows
@@ -18,6 +19,14 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create () {
+    const shuffledEmojis = Phaser.Math.RND.shuffle(Array.from({length: 42 * 44}, (v, i) => i))
+      .slice(0, Math.pow(this.size, 2) / 2)
+
+    const shuffledTiles = Phaser.Math.RND.shuffle(shuffledEmojis.reduce((tiles, number, idx) => {
+      tiles.push(shuffledEmojis[idx], shuffledEmojis[idx])
+      return tiles
+    }, []))
+
     for (let column = 0; column < this.size; column++) {
       const spriteRow = []
       for (let row = 0; row < this.size; row++) {
@@ -25,7 +34,7 @@ export default class MainScene extends Phaser.Scene {
           scene: this,
           x: column * this.tileSize,
           y: row * this.tileSize,
-          frame: 1802
+          actualFrame: shuffledTiles[(column * this.size) + row]
         })
 
         spriteRow.push(sprite)
@@ -39,7 +48,32 @@ export default class MainScene extends Phaser.Scene {
   }
 
   onPointerDown (pointer, sprites,  dragX, dragY) {
-    const sprite = sprites[0]
-    sprite.toggle()
+    const emoji = sprites[0]
+    if (emoji instanceof Emoji) {
+      const similarEmojis = this.openEmojis.filter((openEmoji) => emoji.actualFrame === openEmoji.actualFrame)
+
+      if (similarEmojis.length >= 2) {
+        return
+      }
+
+      emoji.toggle()
+
+      const lastOpenedEmoji = this.openEmojis[this.openEmojis.length - 1]
+      if (similarEmojis[0] || this.openEmojis.length % 2 === 0) {
+        this.openEmojis.push(emoji)
+        return
+      }
+
+      // Disallow clicking momentarily
+      // since we have to show the opened sprites for a while
+      this.input.enabled = false
+      this.openEmojis.pop()
+
+      this.time.delayedCall(500, () => {
+        lastOpenedEmoji.toggle()
+        emoji.toggle()
+        this.input.enabled = true
+      })
+    }
   }
 }
--- src/sprites/emoji.ts
+++ src/sprites/emoji.ts
@@ -2,24 +2,19 @@ import Phaser from 'phaser'
 
 export default class Emoji extends Phaser.GameObjects.Sprite {
   private static readonly closedFrame = 1808
-  private currentFrame: number
+  public actualFrame: number
   private isOpened = false
-  private isPaired = false
 
   constructor (config) {
     super(config.scene, config.x, config.y, 'emoji', Emoji.closedFrame)
     this.scene.add.existing(this)
 
-    this.currentFrame = config.frame
+    this.actualFrame = config.actualFrame
     this.setInteractive()
   }
 
   toggle () {
-    if (this.isPaired) {
-      return
-    }
-
-    this.setFrame(this.isOpened ? Emoji.closedFrame : this.currentFrame)
+    this.setFrame(this.isOpened ? Emoji.closedFrame : this.actualFrame)
     this.isOpened = !this.isOpened
   }
 }

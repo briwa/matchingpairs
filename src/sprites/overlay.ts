2ef7cbba1992b5af3ba1fdc978bc0440ba6b063c
briwa <shabriwa.shalat@gmail.com>
2020-01-27 16:05:45 +0800

    refactor(overlay): use the container itself as an interactive game object

--- src/sprites/overlay.ts
+++ src/sprites/overlay.ts
@@ -11,15 +11,13 @@ export default class Overlay extends Phaser.GameObjects.Container {
     this.scene.add.existing(this)
     const overlay = this.scene.add.rectangle(0, 0, 480, 640, 0xffffff)
       .setOrigin(0, 0)
-      .setInteractive()
 
     const title = this.scene.add
       .text(0, 0, text, this.overlayTextStyle)
       .setAlign('center')
 
     this.add([overlay, title])
-
-    overlay.on('pointerdown', () => this.emit('pointerdown'))
+    this.setInteractive(new Phaser.Geom.Rectangle(0, 0, 480, 640), Phaser.Geom.Rectangle.Contains)
 
     Phaser.Display.Align.In.Center(title, overlay, 0, 0)
   }
